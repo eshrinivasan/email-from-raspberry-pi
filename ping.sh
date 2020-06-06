5c32e411223a52479d4da89fc259847d3bdb210e
Shrinivasan NS <nsshrinivasan@gmail.com>
2020-06-06 23:41:08 +0530

    fixed the path for online.txt

--- ping.sh
+++ ping.sh
@@ -3,7 +3,7 @@ DDMM=`date`
 wget -q --spider https://google.com
 
 if [ $? -eq 0 ]; then
-    echo "Online "$DDMM  > online.txt
+    echo "Online "$DDMM  > /home/pi/PyCode/email-from-raspberry-pi/online.txt
 else
     echo "Offline"
 fi
